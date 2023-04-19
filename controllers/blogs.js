d9422d0285e55e8f3dad1bbaf375e115089a8e0a
Justy Oberg <justyoberg1@gmail.com>
2023-04-19 12:45:49 -0700

    Add functionality for saving each user to each blog and vice versa

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,22 +1,30 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response, next) => {
+  const users = await User.find({})
+  const user = users[0]
+
   const body = request.body
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
+    user: user.id,
     likes: body.likes || 0
   })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog)
+  await user.save()
   response.status(201).json(savedBlog)
 })
 
