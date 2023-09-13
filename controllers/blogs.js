3a031395c745cc6b6a0f70f7aa8707ce6b62534a
Justy Oberg <justyoberg1@gmail.com>
2023-09-12 19:13:26 -0700

    Add route for individual blogs

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,14 +3,21 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog
-    .find({}).populate('user', { username: 1, name: 1 })
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
+})
+
+blogsRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id).populate('user', {
+    username: 1,
+    name: 1,
+  })
+  response.json(blog)
 })
 
 blogsRouter.post('/', userExtractor, async (request, response, next) => {
   const body = request.body
-  
+
   const user = request.user
 
   const blog = new Blog({
@@ -18,7 +25,7 @@ blogsRouter.post('/', userExtractor, async (request, response, next) => {
     author: body.author,
     url: body.url,
     user: user.id,
-    likes: body.likes || 0
+    likes: body.likes || 0,
   })
 
   const savedBlog = await blog.save()
@@ -29,22 +36,24 @@ blogsRouter.post('/', userExtractor, async (request, response, next) => {
 
 blogsRouter.delete('/:id', userExtractor, async (request, response, next) => {
   const blog = await Blog.findById(request.params.id)
-  
+
   if (!blog) {
     response.status(404).json({
-      error: 'blog with that id doesn`t exist'
+      error: 'blog with that id doesn`t exist',
     })
   }
 
   const user = request.user
-  
-  if (blog.user.toString() === user.id.toString()){
+
+  if (blog.user.toString() === user.id.toString()) {
     await Blog.deleteOne({ _id: request.params.id })
     user.blogs = await Blog.find({ user: user.id })
     await user.save()
     response.status(204).end()
   } else {
-    response.status(401).json({ error: 'blog doesn`t belong to logged in user'})
+    response
+      .status(401)
+      .json({ error: 'blog doesn`t belong to logged in user' })
   }
 })
 
@@ -55,12 +64,13 @@ blogsRouter.put('/:id', async (request, response, next) => {
     title: body.title,
     author: body.author,
     url: body.url,
-    likes : body.likes || 0
+    likes: body.likes || 0,
   }
 
-  const updatedBlog =
-    await Blog.findByIdAndUpdate(request.params.id, updated, { new: true })
-    response.status(200).json(updatedBlog)
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, updated, {
+    new: true,
+  })
+  response.status(200).json(updatedBlog)
 })
 
 module.exports = blogsRouter
--- utils/middleware.js
+++ utils/middleware.js
@@ -13,12 +13,12 @@ const requestLogger = (request, response, next) => {
 
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message)
-  
+
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
-  } else if (error.name ===  'JsonWebTokenError') {
+  } else if (error.name === 'JsonWebTokenError') {
     return response.status(401).json({ error: 'invalid id' })
   } else if (error.name === 'TokenExpiredError') {
     return response.status(401).json({ error: 'token expired' })
@@ -38,7 +38,7 @@ const tokenExtractor = (request, response, next) => {
 
 const userExtractor = async (request, response, next) => {
   const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if(!decodedToken.id) {
+  if (!decodedToken.id) {
     return response.status(401).json({ error: 'token invalid' })
   }
   request.user = await User.findOne({ _id: decodedToken.id })
@@ -50,5 +50,5 @@ module.exports = {
   requestLogger,
   errorHandler,
   tokenExtractor,
-  userExtractor
+  userExtractor,
 }
